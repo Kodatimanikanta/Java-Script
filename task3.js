d07f16336934aa189711899ce9440351aa0590a2
Kodatimanikanta <manikodati3@gmail.com>
2024-06-16 11:37:21 +0530

    task 3

---
+++ task3.js
@@ -0,0 +1,69 @@
+// 1.Write a JavaScript function that takes two numbers and an operator ('+', '-', '*', '/') as arguments and returns the result of the arithmetic operation.
+
+//let num1 = 10;
+//let num2 = 10;
+//let sum = num1 + num2;
+//console.log(sum);
+
+Result : 20
+
+//let num1 = 10;
+//let num2 = 10;
+//let sub = num1 - num2;
+//console.log(sub);
+
+Result : 0
+
+//let num1 = 10;
+//let num2 = 10;
+//let mul = num1 * num2;
+//console.log(mul);
+
+Result : 100
+
+//let num1 = 10;
+//let num2 = 10;
+//let div = num1 / num2;
+//console.log(div);
+
+Result : 1
+
+
+// 2. Write a JavaScript function that takes a number as a argument and returns it's square value
+
+
+//let number = 5;
+//let square = number ** 2;
+//console.log(square)
+
+Result : 25
+
+//let number = 30;
+//let squared = Math.pow(number, 2);
+//console.log(squared);
+
+Result : 900
+
+//let number = 5;
+//let cube = number ** 3;
+//console.log(cube)
+
+Result : 125
+
+
+// 3. Write a JavaScript function that takes two numbers as a arguments and returns the highest(max ) of the number using terinary operator inside a function
+
+
+//let max = Math.max(100, 10); 
+//console.log("The Maximum Value is ", max);
+
+Result : 100
+
+//4. Write a javascript function which takes three arguments  (amount , rate of interest, no of years ) returns the total value of interest
+
+
+let P = 100, R = 100, T = 100;
+let SI = (P*R*T)/100;
+console.log(SI)
+
+Result : 10000
